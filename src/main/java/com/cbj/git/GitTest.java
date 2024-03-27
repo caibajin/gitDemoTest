be8636c443e6fbd10998ff7e7dcf711a500edaad
cbj <2254149627@qq.com>
2024-03-27 14:40:56 +0800

    second commit

--- src/main/java/com/cbj/git/GitTest.java
+++ src/main/java/com/cbj/git/GitTest.java
@@ -3,5 +3,7 @@ package com.cbj.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
